984c661f5946b2f7a9ad76082a83ff0bcca45eee
DavidTheCarey <carey2155@gmail.com>
2023-10-30 20:29:24 -0400

    modified home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,15 @@
+import "./HomePage.css"
+
 export default function HomePage() {
     return(
         <div>
             <h1>MadLibs Maker</h1>
-            <main>
-                <div>Welcome to MadLibs Maker, a sight where you can create templates that others can use
+            <main className="homePage">
+                <div>Welcome to MadLibs Maker, a site where you can create templates that others can use
                     to make hilarious madlibs! Or, you can make your own madlibs based off your or other's
                     templates! Check out the links above to get started!
                 </div>
+                <img src="https://i.imgur.com/BCb9boB.png" ></img>
             </main>
         </div>
     );
